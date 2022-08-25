adcfc12363854d621bd7d62cdd9403dc384b6b9a
ryusangwon <r8uuuuuuuu@gmail.com>
2022-08-25 15:07:16 +0900

    solve DFS/BFS

---
+++ DFS_BFS/basic_solve/miro.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+count = 1
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+move = len(dx)
+
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(move):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
