063f2e5ebe40a07c34311b97eb421ed1b47327f3
ryusangwon <r8uuuuuuuu@gmail.com>
2022-08-29 22:42:03 +0900

    solve dynamic programming

---
+++ dynamic_programming/basic_solve/ant_warrior.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int, input().split()))
+d = [0] * 101
+d[0] = arr[0]
+d[1] = max(d[0], arr[1])
+
+for i in range(2, n):
+    d[i] = max(arr[i] + d[i-2], d[i-1])
+
+print(d[n-1])
