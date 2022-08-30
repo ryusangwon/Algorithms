57e33b673c9837396a065cc5de47583ec4a2253a
ryusangwon <r8uuuuuuuu@gmail.com>
2022-08-30 21:14:08 +0900

    solve shortest path

---
+++ shortest_path/basic_solve/dijkstra2.py
@@ -0,0 +1,47 @@
+"""
+heapq > priority queue
+min/max heap, normally min heap
+in python library, use min heap
+heap: min/max value in root node. complete binary tree
+
+no get_smallest_node
+no visited graph
+only distance graph
+"""
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
---
+++ shortest_path/basic_solve/floyd_warshall.py
@@ -0,0 +1,30 @@
+"""
+graph = (n+1) * (n+1), elements INF
+"""
+
+n = int(input())
+m = int(input())
+INF = int(1e9)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
